3a5b54e730da38aafcc9fb534a25414db4545464
Khalid Mahmud <94211717+skhalidmahmud@users.noreply.github.com>
2022-06-05 13:13:57 +0600

    Update oparetor check.c

--- oparetor check.c
+++ oparetor check.c
@@ -2,48 +2,27 @@
 
 int main()
 {
-    char a[4];
+    char a[4] = {0};
     printf("Give me to a operator: ");
-    scanf("%s", &a);
-    if(a == '>='){
-        printf("Greater then or equal to");
-    }else if(a == '>='){
-        printf("Less then or equal to");
-    }else if(a == '++'){
-        printf("Increment");
-    }else if(a == "=="){
-        printf("Equal to");
-    }else if(a == '-'){
-        printf("Subtraction");
-    }else if(a == '+'){
-        printf("Addition");
-    }else if(a == '*'){
-        printf("Multiplication");
-    }else if(a == '/'){
-        printf("Division");
-    }else if(a == '%'){
-        printf("Modulo division");
-    }else if(a == '='){
-        printf("Equal to");
-    }else if(a == '+='){
-        printf("Plus equal to");
-    }else if(a == '-='){
-        printf("Minus equal to");
-    }else if(a == '/='){
-        printf("Division equal to");
-    }else if(a == '*='){
-        printf("Multiplication equal to");
-    }else if(a == '>'){
-        printf("Greater then");
-    }else if(a == '<'){
-        printf("Less then");
-    }else if(a == '&&'){
-        printf("Logical AND");
-    }else if(a == '||'){
-        printf("Logical OR");
-    }else if(a == '!'){
-        printf("Logical NOT");
-    }else
-        printf("It ia not a operator");
+    
+    scanf("%s", a);
+    
+    if(a[0] == '+'){
+        if(a[1] == 0){
+            printf("Addition");
+        }else if(a[1] == '+'){
+            printf("Increment");
+        }else if(a[1] == '='){
+            printf("Plus equal to");
+        }
+    }else if(a[0] == '-'){
+        if(a[1] == 0){
+            printf("Subtraction");
+        }else if(a[1] == '-'){
+            printf("Decrement");
+        }else if(a[1] == '='){
+            printf("Minus equal to");
+        }
+    }
     return 0;
 }
