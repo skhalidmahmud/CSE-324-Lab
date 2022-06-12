ed91658f92a184c3616febbaa3692a124563f856
Khalid Mahmud <94211717+skhalidmahmud@users.noreply.github.com>
2022-06-12 13:14:08 +0600

    Create in a sentence all value count.c

---
+++ in a sentence all value count.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    char str[100] = {'\n'}, c, ans[100] = {0};
+    printf("Give me a sentence: ");
+    fgets(str, 100, stdin);
+    
+    for(int i = 0; str[i] == '\n'; i++){
+        if(str[i] >= 0 && str[i] <= 128){
+            ans[i] = 
+        }
+    }
+    getch();
+    return 0;
+}
