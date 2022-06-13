d72f3c7972ac99ef16a496b84294010fff64803c
Khalid Mahmud <94211717+skhalidmahmud@users.noreply.github.com>
2022-06-14 01:41:16 +0600

    Update in a sentence all value count.c

--- in a sentence all value count.c
+++ in a sentence all value count.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
 int main(){
-    char str[100] = {'\n'}, c, ans[100] = {0};
+    char str[100];
+    int ans[128]={0};
     printf("Give me a sentence: ");
     fgets(str, 100, stdin);
-    
-    for(int i = 0; str[i] == '\n'; i++){
-        if(str[i] >= 0 && str[i] <= 128){
-            ans[str[i] += 1;
+
+    for(int i=0; i<=strlen(str); i++){
+        ans[(int)str[i]] = ans[(int)str[i]] + 1;
+    }
+
+    printf("\nNumber of word is:\n");
+    for(int i=30; i<=127; i++){
+        if(!(ans[i]==0)){
+            printf("   %c :  %d\n",(char)i, ans[i]);
         }
     }
+
     getch();
     return 0;
 }
