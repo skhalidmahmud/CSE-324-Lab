eae55ce8a625999303bd6647bfa0e691985aee52
Khalid Mahmud <94211717+skhalidmahmud@users.noreply.github.com>
2022-06-14 02:08:30 +0600

    Update For counting the number of alphabets from a sentence.c

--- For counting the number of alphabets from a sentence.c
+++ For counting the number of alphabets from a sentence.c
@@ -2,7 +2,7 @@
 
 int main(){
     char str[100];
-    int ans[128]={0};
+    int ans[127]={0};
     printf("Give me a sentence: ");
     fgets(str, 100, stdin);
 
@@ -10,10 +10,10 @@ int main(){
         ans[(int)str[i]] = ans[(int)str[i]] + 1;
     }
 
-    printf("\nNumber of word is:\n");
-    for(int i=30; i<=127; i++){
+    printf("\nNumber of alphabet's is:\n");
+    for(int i=31; i<=126; i++){
         if(!(ans[i]==0)){
-            printf("   %c :  %d\n",(char)i, ans[i]);
+            printf("  %c : %d\n",(char)i, ans[i]);
         }
     }
 
